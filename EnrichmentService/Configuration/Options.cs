f1a626eb780afac05940ba711edb66fa8c7439ae
Igor Valukhov <Igor.Valukhov@gmail.com>
2026-03-06 10:25:27 +0300

    Added options for kafka

---
+++ EnrichmentService/Configuration/Options.cs
@@ -0,0 +1,11 @@
+﻿namespace EnrichmentService.Configuration;
+
+public sealed class KafkaOptions
+{
+    public const string SectionName = "Kafka";
+
+    public required string BootstrapServers { get; init; }
+    public required string InputTopic { get; init; }
+    public required string ConsumerGroup { get; init; }
+    public int PollTimeoutMs { get; init; } = 1000;
+}
